12c4c9c9ec5f28caf2a7e8ec53b36a2e07e9f7a6
Andreas Vieten <cryptonari@outlook.com>
2022-05-25 22:21:53 +0200

    starter code

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { App } from './js/App';
+import { Game } from './js/App';
 import './styles/App.scss'
+import './js/script'
 
 
 const container = document.getElementById('app');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App tab="home" />);
+root.render(<Game />);
--- src/client/js/App.js
+++ src/client/js/App.js
@@ -1,10 +1,57 @@
 import React from 'react';
 
-export function App() {
-    return (
+class Square extends React.Component {
+    render() {
+      return (
+        <button className="square">
+          {/* TODO */}
+        </button>
+      );
+    }
+  }
+  
+  class Board extends React.Component {
+    renderSquare(i) {
+      return <Square />;
+    }
+  
+    render() {
+      const status = 'Next player: X';
+  
+      return (
         <div>
-            <h1>React-Webpack boilerplate</h1>
-            <p>Today's Date: { new Date().toDateString() }</p>
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+      );
+    }
+  }
+  
+
+export function Game() {
+    return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
         </div>
     );
 }
