9cc20c6285a010e352417ed5a080cf23ad4dbbff
Andreas Vieten <cryptonari@outlook.com>
2022-05-25 22:06:59 +0200

    refactor code

--- src/client/js/App.js
+++ src/client/js/App.js
@@ -3,7 +3,8 @@ import React from 'react';
 export function App() {
     return (
         <div>
-            <h1>Welcome Date is: { new Date().toDateString() }</h1>
+            <h1>React-Webpack boilerplate</h1>
+            <p>Today's Date: { new Date().toDateString() }</p>
         </div>
     );
 }
