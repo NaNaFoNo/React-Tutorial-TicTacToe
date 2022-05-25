1bee685320872a0840d3620c7efad727871a2d26
Andreas Vieten <cryptonari@outlook.com>
2022-05-25 23:47:09 +0200

    Lifting state up Board to Game

--- src/client/index.js
+++ src/client/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Game } from './js/App';
+import { App } from './js/App';
 import './styles/App.scss'
 import './js/script'
 
 
 const container = document.getElementById('app');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<Game />);
+root.render(<App />);
