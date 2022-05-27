a5984c516c6460e35e2231e5b2bf3b22e738367f
Andreas Vieten <cryptonari@outlook.com>
2022-05-27 19:10:54 +0200

    add Game Draw message

--- src/client/js/App.js
+++ src/client/js/App.js
@@ -153,7 +153,12 @@ class Game extends React.Component {
     if (winner) {
       status = 'Winner: ' + winner;
     } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      if (this.state.stepNumber === 9) {
+        status = 'Match ended in a draw!';
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
+      
     }
 
     return (
